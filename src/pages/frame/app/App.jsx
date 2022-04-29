233b3419258c9a02bd86ad3c347c1a8d004a7f2c
zhaozhiyuan@hcfysz.com <zhaozhiyuan@hcfysz.com>
2022-04-29 15:18:05 +0800

    11

---
+++ src/pages/frame/app/App.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { HashRouter } from "react-router-dom";
+import MyRoutes from "@/routes";
+import "./App.less";
+
+const App = () => {
+    const a;
+    a =b
+    return (
+        <HashRouter>
+            <MyRoutes />
+        </HashRouter>
+    );
+};
+
+export default App;
